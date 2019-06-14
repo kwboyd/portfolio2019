7c4b84407e536d7ada9f41fa9af7676a9f2936f6
Kate Boyd <kwboyd95@gmail.com>
2019-06-13 18:28:47 -0600

    Add colors, set up gradients

--- src/header/Header.js
+++ src/header/Header.js
@@ -4,9 +4,14 @@ import './Header.scss';
 const Header = () => {
     return (
         <section className="header">
+            <div className="gradient" id="gradient-2"></div>
+            <div className="gradient" id="gradient-3"></div>
+            <div className="gradient" id="gradient-4"></div>
             <div className="header-text">
-                <h1>Kate Boyd</h1>
-                <h2>Software Engineer</h2>
+                <div>
+                    <h1>Kate Boyd</h1>
+                    <h2>Software Engineer</h2>
+                </div>
             </div>
         </section>
     )
