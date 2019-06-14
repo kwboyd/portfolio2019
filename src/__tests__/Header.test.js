617a1df5cc34efbfe2d1aacce8e646624d9d706e
Kate Boyd <kwboyd95@gmail.com>
2019-06-14 15:59:02 -0600

    Remove unnecessary tests

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -9,11 +9,6 @@ import Header from '../header/Header';
 
 afterEach(cleanup)
 
-test('renders three gradient divs', () => {
-    const {getAllByTestId} = render(<Header />)
-    expect(getAllByTestId('gradient').length).toBe(3);
-})
-
 test('renders Kate Boyd', () => {
     const {getByText} = render(<Header />)
     expect(getByText('Kate Boyd')).toBeTruthy();
