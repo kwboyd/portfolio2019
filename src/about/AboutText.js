f489886dd19b43556994b9f8cf49fccd5139baad
Kate Boyd <kwboyd95@gmail.com>
2019-06-17 18:42:56 -0600

    Update site with new bio text

--- src/about/AboutText.js
+++ src/about/AboutText.js
@@ -6,8 +6,7 @@ const AboutText = () => {
             <h3 data-testid="intro">Hi! I'm a full-stack developer in Denver, CO.</h3>
             <p>I've built a mobile app from scratch with thousands of users that later was acquired by a larger gaming company.</p>
             <p>I've built a website that won multiple awards for its interactive storytelling.</p>
-            <p>I've worked in an Agile environment as the sole front end engineer for a company portal that brings in over $100 million each year.</p>
-            <p>JavaScript and its frameworks are my main tools of the trade.</p>
+            <p>I have a degree in Journalism & Japanese. I like creative writing, dancing, cats, coffee, and JavaScript.</p>
         </div>
     )
 }
