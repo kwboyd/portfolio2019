ff07ab1322353636f5abb84c3947800bb1424dac
Kate Boyd <kwboyd95@gmail.com>
2022-01-30 11:53:25 -0700

    Add Shopify work changes

--- src/about/AboutText.js
+++ src/about/AboutText.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AboutText = () => {
     return (
         <div className="about-text content-column">
-            <h3 data-testid="intro">Hi! I'm a full-stack developer in Denver, CO.</h3>
+            <h3 data-testid="intro">Hi! I'm a full-stack developer in Denver, CO. I currently work on the Deploys team at Shopify.</h3>
             <p>I've built a mobile app from scratch with thousands of users that later was acquired by a larger gaming company.</p>
             <p>I've built a website that won multiple awards for its interactive storytelling.</p>
             <p>I have a degree in Journalism & Japanese. I like creative writing, dancing, cats, coffee, and JavaScript.</p>
